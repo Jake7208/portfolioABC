8ccfb6904412aab3a8103763f1a02d40609e96cf
Jake7208 <jake7208@gmail.com>
2023-06-26 21:48:19 -0600

    Parallax effect acquired
    lots of work with phots and stuff

---
+++ script.js
@@ -0,0 +1,17 @@
+let trees = document.getElementById("trees");
+let island = document.getElementById("island");
+let tree2 = document.getElementById("tree2");
+let balloons = document.getElementById("balloons");
+let lower = document.getElementById("lower");
+let lowerDark = document.getElementById("lowerDark");
+let next = document.getElementById("next");
+
+window.addEventListener("scroll", () => {
+  let value = window.scrollY;
+
+  trees.style.top = value * 0.9 + "px";
+  island.style.top = value * 0.7 + "px";
+  balloons.style.left = value * 1 + "px";
+  tree2.style.top = value * 0.5 + "px";
+  lower.style.top = value * 0.7 + "px";
+});
